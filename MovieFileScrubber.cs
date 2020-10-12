1c2aa9c94f5a0403385cc99b9b3b0224b4e01b32
jon walker <timmyburner888@gmail.com>
2020-10-12 03:23:19 -0500

    other

--- MovieFileScrubber.cs
+++ MovieFileScrubber.cs
@@ -12,38 +12,36 @@ namespace MediaLibrary
         {
             try
             {
-                // determine name of writeFile
+                
                 string ext = readFile.Split('.').Last();
                 string writeFile = readFile.Replace(ext, $"scrubbed.{ext}");
-                // if writeFile exists, the file has already been scrubbed
+               
                 if (File.Exists(writeFile))
                 {
-                    // file has already been scrubbed
+                    
                     logger.Info("File already scrubbed");
                 }
                 else
                 {
-                    // file has not been scrubbed
+                    
                     logger.Info("File scrub started");
-                    // open write file
+                 
                     StreamWriter sw = new StreamWriter(writeFile);
-                    // open read file
+                    
                     StreamReader sr = new StreamReader(readFile);
-                    // remove first line - column headers
+                  
                     sr.ReadLine();
                     while (!sr.EndOfStream)
                     {
-                        // create instance of Movie class
+                        
                         Movie movie = new Movie();
                         string line = sr.ReadLine();
-                        // look for quote(") in string
-                        // this indicates a comma(,) or quote(") in movie title
+                       
                         int idx = line.IndexOf('"');
                         string genres = "";
                         if (idx == -1)
                         {
-                            // no quote = no comma or quote in movie title
-                            // movie details are separated with comma(,)
+                            
                             string[] movieDetails = line.Split(',');
                             movie.mediaId = UInt64.Parse(movieDetails[0]);
                             movie.title = movieDetails[1];
@@ -53,24 +51,23 @@ namespace MediaLibrary
                         }
                         else
                         {
-                            // quote = comma or quotes in movie title
-                            // extract the movieId
+                            
                             movie.mediaId = UInt64.Parse(line.Substring(0, idx - 1));
-                            // remove movieId and first comma from string
+                            
                             line = line.Substring(idx);
-                            // find the last quote
+                            
                             idx = line.LastIndexOf('"');
-                            // extract title
+                            
                             movie.title = line.Substring(0, idx + 1);
-                            // remove title and next comma from the string
+                           
                             line = line.Substring(idx + 2);
-                            // split the remaining string based on commas
+                           
                             string[] details = line.Split(',');
-                            // the first item in the array should be genres 
+                      
                             genres = details[0];
-                            // if there is another item in the array it should be director
+                   
                             movie.director = details.Length > 1 ? details[1] : "unassigned";
-                            // if there is another item in the array it should be run time
+                
                             movie.runningTime = details.Length > 2 ? TimeSpan.Parse(details[2]) : new TimeSpan(0);
                         }
                         sw.WriteLine($"{movie.mediaId},{movie.title},{genres},{movie.director},{movie.runningTime}");
