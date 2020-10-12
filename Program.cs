50c49e6cf5d1b47a0594019f43dc930dc0d5a23e
jon walker <timmyburner888@gmail.com>
2020-10-12 03:22:51 -0500

    Media Library Lab

--- Program.cs
+++ Program.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using NLog.Web;
 using System.IO;
-using System.Collections.Generic;
 
-namespace MovieLibrary
+namespace MediaLibrary
 {
     class Program
     {
@@ -11,66 +10,46 @@ namespace MovieLibrary
         private static NLog.Logger logger = NLogBuilder.ConfigureNLog(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
         static void Main(string[] args)
         {
-            string movieFilePath = Directory.GetCurrentDirectory() + "\\movies.csv";
 
             logger.Info("Program started");
 
-            MovieFile movieFile = new MovieFile(movieFilePath);
+            Movie movie = new Movie
+         {
+            mediaId = 123,
+                title = "Greatest Movie Ever, The (2020)",
+                director = "Jeff Grissom",
+            
+              runningTime = new TimeSpan(2, 21, 23),
+            genres = { "Comedy", "Romance" }
+            };
 
-            string choice = "";
-            do
-            {
-                
-                Console.WriteLine("1) Add Movie");
-                Console.WriteLine("2) Display All Movies");
-                Console.WriteLine("Enter to quit");
-                
-                choice = Console.ReadLine();
-                logger.Info("User choice: {Choice}", choice);
+             Console.WriteLine(movie.Display());
 
-                if (choice == "1")
-                {
-                    
-                    Movie movie = new Movie();
-                    
-                    Console.WriteLine("Enter movie title");
-                    
-                    movie.title = Console.ReadLine();
-                    
-                    if (movieFile.isUniqueTitle(movie.title)){
-                        
-                        string input;
-                        do
-                        {
-                            
-                            Console.WriteLine("Enter genre (or done to quit)");
-                            
-                            input = Console.ReadLine();
-                            
-                            if (input != "done" && input.Length > 0)
-                            {
-                                movie.genres.Add(input);
-                            }
-                        } while (input != "done");
-                        
-                        if (movie.genres.Count == 0)
-                        {
-                            movie.genres.Add("(no genres listed)");
-                        }
-                        
-                        movieFile.AddMovie(movie);
-                    }
-                } else if (choice == "2")
-                {
-                    
-                    foreach(Movie m in movieFile.Movies)
-                    {
-                        Console.WriteLine(m.Display());
-                    }
-                }
-            } while (choice == "1" || choice == "2");
+             Album album = new Album
+             {
+                 mediaId = 321,
+                 title = "Greatest Album Ever, The (2020)",
+                 artist = "Jeff's Awesome Band",
+                 recordLabel = "Universal Music Group",
+                genres = { "Rock" }
+            };
+         Console.WriteLine(album.Display());
+
+             Book book = new Book
+             {
+                 mediaId = 111,
+                 title = "Super Cool Book",
+                 author = "Jeff Grissom",
+                 pageCount = 101,
+                 publisher = "",
+                genres = { "Suspense", "Mystery" }
+             };
+             Console.WriteLine(book.Display());
+
+            string scrubbedFile = FileScrubber.ScrubMovies("movies.csv");
+            logger.Info(scrubbedFile);
 
             logger.Info("Program ended");
         }
     }
-} 
+}
